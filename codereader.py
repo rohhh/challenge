524162c851b1da3c4e00dc55c921de7373f95683
ro <roandradem@gmail.com>
2020-10-08 18:54:10 -0300

    Programa funcionando al 100%

--- codereader.py
+++ codereader.py
@@ -12,15 +12,14 @@ if os.path.isdir(folder)==False:
 
 cap = cv2.VideoCapture(1)
 
+registeredCodes = []
+count = 0
+
 while True:
 
-    count = 0
     success, img = cap.read()
-    #img2=cv2.imread("qrcodes3.jpg")
-    #img=cv2.resize(img2,(640,480))   
-    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    #print(decode(imgGray))
-    
+    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)  
+
     for barcode in decode(imgGray):
         procDay=str(time.localtime()[2])+"/"+str(time.localtime()[1])+"/"+str(time.localtime()[0])
         procHour=str(time.localtime()[3])+":"+str(time.localtime()[4])+":"+str(time.localtime()[5])
@@ -29,8 +28,8 @@ while True:
         addMarginL = 40
         addMarginR = 40
         addMarginB = 40
-        scale=5
-        pointsList= []
+        scale = 5
+        pointsList = []
 
         (x, y, w, h) = barcode.rect
         barType=barcode.type
@@ -46,20 +45,28 @@ while True:
         scaledImg = cv2.resize(croppedImg,(croppedImg.shape[1]*scale,croppedImg.shape[0]*scale))
         if barType=="QRCODE":
             for i in range(len(pointsList)):
+                cv2.line(img, pointsList[i], pointsList[i-1], (255, 0, 0), 2)
                 cv2.line(scaledImg, ((pointsList[i][0]-x+addMarginL)*scale,(pointsList[i][1]-y+addMarginT)*scale), ((pointsList[i-1][0]-x+addMarginL)*scale,(pointsList[i-1][1]-y+addMarginT)*scale), (255, 0, 0), 2)     
         else:
+            for i in range(len(pointsList)):
+                cv2.line(img, pointsList[i], pointsList[i-1], (0, 125, 255), 1)
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
             cv2.rectangle(scaledImg, (addMarginL*scale, addMarginT*scale), ((addMarginL + w)*scale, (addMarginT + h)*scale), (255, 0, 0), 2)
-        cv2.putText(scaledImg,procDay+"-"+procHour,(10,25),cv2.FONT_HERSHEY_TRIPLEX,0.6,(0,230,0),1)
-        cv2.putText(scaledImg,codeData,(10,50),cv2.FONT_HERSHEY_TRIPLEX,0.6,(0,230,0),1)
-        cv2.putText(img,procDay+"-"+procHour,(10,25),cv2.FONT_HERSHEY_TRIPLEX,0.5,(0,230,0),1)
-        cv2.putText(img,codeData,(10,45),cv2.FONT_HERSHEY_TRIPLEX,0.5,(0,230,0),1)      
-        #cv2.imshow("Cropped",scaledImg)
-        for i in range(len(pointsList)):
-            cv2.line(img, pointsList[i], pointsList[i-1], (255, 0, 0), 2)
-        cv2.imwrite("saved/code"+str(count)+".jpg",scaledImg)
-        cv2.imwrite("saved/code"+str(count)+"_full.jpg",img)
-        count += 1
-
+        cv2.putText(scaledImg,procDay+"-"+procHour,(10,25),cv2.FONT_HERSHEY_TRIPLEX,0.6,(225,0,0),1)
+        cv2.putText(scaledImg,barType+" : "+codeData,(10,50),cv2.FONT_HERSHEY_TRIPLEX,0.6,(225,0,0),1)
+        #cv2.putText(img,procDay+"-"+procHour,(10,25),cv2.FONT_HERSHEY_TRIPLEX,0.5,(125,0,0),1)
+        #cv2.putText(img,barType+" : "+codeData,(10,45),cv2.FONT_HERSHEY_TRIPLEX,0.5,(125,0,0),1)      
+#        cv2.imshow("Cropped",scaledImg)
+#        for i in range(len(pointsList)):
+#            cv2.line(img, pointsList[i], pointsList[i-1], (255, 0, 0), 2)
+        if codeData not in registeredCodes:
+            registeredCodes.append(codeData)
+            count += 1
+            print(str(count)+" saved code(s): "+codeData)
+            cv2.imwrite("saved/code"+str(count)+".jpg",scaledImg)
+            #cv2.imwrite("saved/code"+str(count)+"_full.jpg",img)
+            
+    cv2.putText(img,str(count)+" registered codes.",(10,25),cv2.FONT_HERSHEY_TRIPLEX,0.5,(125,0,0),1)
     cv2.imshow("Codes",img)
     cv2.waitKey(1)
     if cv2.waitKey(1) & 0xFF==ord('q'):
